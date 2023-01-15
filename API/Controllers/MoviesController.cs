fd879f72c239ee37e1fba7230159a5230165aa0f
Gonzalo Menacho <menachoga@gmail.com>
2023-01-14 21:01:26 -0500

    Added reviews-frontend

---
+++ API/Controllers/MoviesController.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IElasticClient _elasticClient;
+        // create elasticClient field thru injection
+        public MoviesController(IElasticClient elasticClient)
+        {
+            _elasticClient = elasticClient;
+
+        }
+        [HttpGet] //api/movies
+        public async Task<ActionResult<List<Movie>>> GetMovies()
+        {
+            var response = await _elasticClient.SearchAsync<Movie>(s => s
+                .Index("movies")
+                .Query(q => q.MatchAll()));
+            // returns all movies
+
+            return response.Documents.ToList();
+        }
+
+        // return movie based on the movie name path
+        // pass index name here if we want to deal with different indices
+        [HttpGet("{title}")] //api/movies/movieX
+        public async Task<Movie> Get(string title)
+        {
+            var response = await _elasticClient.SearchAsync<Movie>(s => s
+                .Index("movies") // index name
+                .Query(q => q.Term(t => t.Title, title) || q.Match(m => m.Field(f => f.Title).Query(title)))); // term allows to find docs matching an exact query
+                                                                                                               // match allows for the user to enter in some text that text to match any part of the content in the document
+
+            return response.Documents.FirstOrDefault();
+        }
+
+        [HttpPost("add")]
+        public async Task<string> Post(Movie value)
+        {
+            var response = await _elasticClient.IndexAsync<Movie>(value, x => x.Index("movies")); // pass incoming value and index
+            return response.Id; // Id created when making a post call
+        }
+
+        [HttpDelete("del/{id}")]
+        // delete based on id (http://localhost:9200/movies/_search) -> find id
+        public async void Delete(string id)
+        {
+            var response = await _elasticClient.DeleteAsync<Movie>(id, d => d
+              .Index("movies"));
+
+        }
+
+        [HttpPut("edit/{id}")]
+        public async Task<Movie> Put(string id, string title, float userrating, string description, string criticrating, double totalratingcount, string totaluserreviews,
+            string totalcriticreviews, List<string> genres, string datepublished, int duration, string movietrailer)
+        {
+
+            var response = await _elasticClient.UpdateAsync<Movie>(id, u => u
+                .Index("movies")
+                .Doc(new Movie { Title = title, UserRating = userrating, Description = description, CriticRating = criticrating, TotalRatingCount = totalratingcount, TotalUserReviews = totaluserreviews,
+                TotalCriticReviews = totalcriticreviews, Genres = genres, DatePublished = datepublished, Duration = duration, MovieTrailer = movietrailer}));
+
+            return null;
+        }
+
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,47 @@
+using System.Net;
+using API;
+using Microsoft.AspNetCore.HttpOverrides;
+using Nest;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Elasticsearch services
+
+var settings = new ConnectionSettings(); // takes http://localhost:9200 as default uri
+// create elasticsearch connection
+builder.Services.AddSingleton<IElasticClient>(new ElasticClient(settings));
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(opt =>
+    {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
+            policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+        });
+    });
+
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("CorsPolicy");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
