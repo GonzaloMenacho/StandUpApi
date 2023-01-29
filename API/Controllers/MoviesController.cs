20899f836d4ae02bb1088e4150f39a5a2ab2578c
Elijah Nieves <elijahnieves328@gmail.com>
2023-01-28 22:35:30 -0500

    Updated movie controller to match up with the elastic schema.

--- API/Controllers/MoviesController.cs
+++ API/Controllers/MoviesController.cs
@@ -7,6 +7,7 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class MoviesController : ControllerBase
     {
+        private readonly string movieIndex = "movies";
         private readonly IElasticClient _elasticClient;
         // create elasticClient field thru injection
         public MoviesController(IElasticClient elasticClient)
@@ -18,7 +19,7 @@ namespace API.Controllers
         public async Task<ActionResult<List<Movie>>> GetMovies()
         {
             var response = await _elasticClient.SearchAsync<Movie>(s => s
-                .Index("movies")
+                .Index(movieIndex)
                 .Query(q => q.MatchAll()));
             // returns all movies
 
@@ -31,7 +32,7 @@ namespace API.Controllers
         public async Task<Movie> Get(string title)
         {
             var response = await _elasticClient.SearchAsync<Movie>(s => s
-                .Index("movies") // index name
+                .Index(movieIndex) // index name
                 .Query(q => q.Term(t => t.Title, title) || q.Match(m => m.Field(f => f.Title).Query(title)))); // term allows to find docs matching an exact query
                                                                                                                // match allows for the user to enter in some text that text to match any part of the content in the document
 
@@ -41,7 +42,7 @@ namespace API.Controllers
         [HttpPost("add")]
         public async Task<string> Post(Movie value)
         {
-            var response = await _elasticClient.IndexAsync<Movie>(value, x => x.Index("movies")); // pass incoming value and index
+            var response = await _elasticClient.IndexAsync<Movie>(value, x => x.Index(movieIndex)); // pass incoming value and index
             return response.Id; // Id created when making a post call
         }
 
@@ -50,19 +51,19 @@ namespace API.Controllers
         public async void Delete(string id)
         {
             var response = await _elasticClient.DeleteAsync<Movie>(id, d => d
-              .Index("movies"));
+              .Index(movieIndex));
 
         }
 
         [HttpPut("edit/{id}")]
-        public async Task<Movie> Put(string id, string title, float userrating, string description, string criticrating, double totalratingcount, string totaluserreviews,
-            string totalcriticreviews, List<string> genres, string datepublished, int duration, string movietrailer)
+        public async Task<Movie> Put(int id, string title, float userrating, string description, string criticrating, double totalratingcount, string totaluserreviews,
+            string totalcriticreviews, string[] genres, string datepublished, int duration, string movietrailer, string movieposter)
         {
 
             var response = await _elasticClient.UpdateAsync<Movie>(id, u => u
-                .Index("movies")
-                .Doc(new Movie { Title = title, UserRating = userrating, Description = description, CriticRating = criticrating, TotalRatingCount = totalratingcount, TotalUserReviews = totaluserreviews,
-                TotalCriticReviews = totalcriticreviews, Genres = genres, DatePublished = datepublished, Duration = duration, MovieTrailer = movietrailer}));
+                .Index(movieIndex)
+                .Doc(new Movie { Id = id, Title = title, movieIMDbRating = userrating, Description = description, MetaScore = criticrating, TotalRatingCount = totalratingcount, TotalUserReviews = totaluserreviews,
+                TotalCriticReviews = totalcriticreviews, MovieGenres = genres, DatePublished = datepublished, Duration = duration, MovieTrailer = movietrailer, MoviePoster=movieposter}));
 
             return null;
         }
