19dbc350fb427f214d7e788fd064d056f1ff3ad8
Michael DAgostino <dagostinomichael00@gmail.com>
2023-04-01 03:14:50 -0400

    Further refactoring to search route and BasicSearchService.cs.

--- API/services/BasicSearchService.cs
+++ API/services/BasicSearchService.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Nest;
+using System.Threading.Tasks;
 
 namespace API.services
 {
     public class BasicSearchService
     {
+        private static readonly string movieIndex = "movies";
+        private static readonly string reviewIndex = "reviews";
+
         public static async Task<ActionResult<MovieReview>> GetMovieReviewFromTerm(IElasticClient _elasticClient, string? term = null)
         {
             List<Movie> movielist = new List<Movie>();
@@ -14,11 +18,8 @@ namespace API.services
             {
                 // get all the movie documents in the db
                 // returns 10 movies
-                var response = await _elasticClient.SearchAsync<Movie>(s => s
-                    .Index("movies")
-                    .Size(10)
-                    .Query(q => q.MatchAll())
-                    );
+                var response = await MatchAllQuery(_elasticClient, movieIndex, 10);
+
                 if (!response.IsValid)  // if we didn't reach the database for the movies
                 {
                     throw new HttpRequestException("Failed to MatchAll movies in BasicSearchService.");
@@ -29,20 +30,13 @@ namespace API.services
                 foreach (Movie movie in movielist) // for each movie in our movie list
                 {
                     string[] movieidarr = { movie.MovieID.ToString() };
-                    Console.WriteLine(movieidarr);
-                    var res = await _elasticClient.SearchAsync<Review>(s => s
-                        .Index("reviews")   // https://tenor.com/view/now-whos-gonna-stop-me-bowser-the-super-mario-bros-movie-try-to-stop-me-now-im-unstoppable-gif-26913745
-                        .Size(3)            // limits the number of hits to 3
-                        .Query(q => matchService.MatchRequest("movieID", reviewOBJ, movieidarr))
-                        .Sort(sort => sort
-                            .Descending("_score")           // this sorts by relevancy
-                        //.Descending(f => f.UsefulnessVote)    // this sorts by usefulness votes
-                        )
-                        );
+                    var res = await MatchSearchQuery(_elasticClient, new Review(), "movieID", movieidarr);
+
                     if (!res.IsValid)   // if we didn't get the review json back
                     {
                         throw new HttpRequestException("Failed to retrieve reviews in BasicSearchService with an empty string.");
                     }
+
                     reviewlist.Add(res.Documents.ToList()); // append the response to the movie list.
                 }
             }
@@ -52,7 +46,7 @@ namespace API.services
                 Movie movieobj = new Movie();
                 string[] termsList = { term };      // unecessarily needed, but absolutely required
                 var response = await _elasticClient.SearchAsync<Movie>(s => s
-                    .Index("movies")
+                    .Index(movieIndex)
                     .Query(q =>
                         searchByCharRaw.RegexpRequest("title", movieobj, termsList)
                         )
@@ -67,13 +61,7 @@ namespace API.services
                 {
                     // get all the movie documents in the db
                     // returns 10 movies
-                    var res = await _elasticClient.SearchAsync<Movie>(s => s
-                        .Index("movies")
-                        .Size(10)
-                        .Query(q => q
-                            .MatchAll()
-                            )
-                        );
+                    var res = await MatchAllQuery(_elasticClient, movieIndex, 10);
                     if (!res.IsValid)  // if we didn't reach the database for the movies
                     {
                         throw new HttpRequestException("Failed to MatchAll movies in BasicSearchService after finding no results using term.");
@@ -88,16 +76,7 @@ namespace API.services
                 {
                     foreach (Movie movie in movielist)
                     {
-                        var res = await _elasticClient.SearchAsync<Review>(s => s
-                            .Index("reviews")
-                            .Query(q => WeightedReviewQuery(term, movie))
-                            .Size(3)
-                            .Sort(sort => sort
-                                .Descending("_score")           // this sorts by relevancy
-                                //.Descending(f => f.UsefulnessVote)    // this sorts by usefulness votes
-                                )
-                            );
-
+                        var res = await GetWeightedReviewQuery(_elasticClient, movie, term, reviewIndex, 3);
                         if (!res.IsValid)
                         {
                             throw new HttpRequestException("Failed to find reviews in BasicSearchService function.");
@@ -120,8 +99,118 @@ namespace API.services
             return movieReview;   // return the MR object with status 200
         }
 
-        public static QueryContainer WeightedReviewQuery(string term, Movie movie)
+
+        public static async Task<ISearchResponse<Movie>> MatchAllQuery(IElasticClient _elasticClient, string index, int? size = 10)
         {
+            if (_elasticClient is null)
+            {
+                throw new ArgumentNullException(nameof(_elasticClient));
+            }
+
+            if (index is null)
+            {
+                throw new ArgumentNullException(nameof(index));
+            }
+
+            var response = await _elasticClient.SearchAsync<Movie>(s => s
+                .Index(index)
+                .Size(size)
+                .Query(q => q
+                    .MatchAll())
+                );
+            return response;
+        }
+
+        public static async Task<ISearchResponse<T>> MatchSearchQuery<T>(
+            IElasticClient _elasticClient,
+            T typeOBJ,
+            string field,
+            string[] termArray,
+            int? size = 10,
+            string? sortorder = "_score") where T : class
+        {
+            if (_elasticClient is null)
+            {
+                throw new ArgumentNullException(nameof(_elasticClient));
+            }
+
+            if (typeOBJ is null)
+            {
+                throw new ArgumentNullException(nameof(typeOBJ));
+            }
+
+            if (field is null)
+            {
+                throw new ArgumentNullException(nameof(field));
+            }
+
+            if (termArray is null)
+            {
+                throw new ArgumentNullException(nameof(termArray));
+            }
+
+            object ModelOBJ = null;
+            string index = null;
+            switch (typeOBJ)
+            {
+                case Movie: ModelOBJ = new Movie(); index = movieIndex; break;
+                case Review: ModelOBJ = new Review(); index = reviewIndex; break;
+                default: throw new ArgumentException("You need to pass in a valid empty object (Movie/Review) into MatchSearchQuery!");
+            }
+
+            var response = await _elasticClient.SearchAsync<T>(s => s
+                .Index(index)   // https://tenor.com/view/now-whos-gonna-stop-me-bowser-the-super-mario-bros-movie-try-to-stop-me-now-im-unstoppable-gif-26913745
+                .Size(size)            // limits the number of hits to 3 if "3" is input
+                .Query(q => matchService.MatchRequest(field, ModelOBJ, termArray))
+                .Sort(sort => sort
+                    .Descending(sortorder)           // this sorts by relevancy if "_score" input
+                    )
+                );
+            return response;
+        }
+
+        public static async Task<ISearchResponse<Review>> GetWeightedReviewQuery(
+            IElasticClient _elasticClient, 
+            Movie movie, 
+            string term = "",
+            string? index = "reviews", 
+            int? size = 3,
+            string? sortorder = "_score")
+        {
+            if (_elasticClient is null)
+            {
+                throw new ArgumentNullException(nameof(_elasticClient));
+            }
+
+            if (movie is null)
+            {
+                throw new ArgumentNullException(nameof(movie));
+            }
+
+            var response = await _elasticClient.SearchAsync<Review>(s => s
+                .Index(index)
+                .Query(q => GetWeightedReviewQueryContainer(term, movie))
+                .Size(size)
+                .Sort(sort => sort
+                    .Descending(sortorder)           // this sorts by relevancy
+                    //.Descending(f => f.UsefulnessVote)    // this sorts by usefulness votes
+                    )
+                );
+            return response;
+        }
+
+        public static QueryContainer GetWeightedReviewQueryContainer(string term, Movie movie)
+        {
+            if (term is null)
+            {
+                throw new ArgumentNullException(nameof(term));
+            }
+
+            if (movie is null)
+            {
+                throw new ArgumentNullException(nameof(movie));
+            }
+
             return Query<Review>.FunctionScore(fs => fs
                 .Query(q2 => q2
                     .MultiMatch(m => m
@@ -158,64 +247,3 @@ namespace API.services
         }
     }
 }
-
-
-/*
-             try
-            {
-                var movieReview = await BasicSearchService.GetMovieReviewFromTerm(_elasticClient, term);
-                return Ok(movieReview.Value);   // return the MR object with status 200
-            }
-            catch (Exception e)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
-            }
-*/
-
-/*
-                            .Query(q => q
-                                .FunctionScore(fs => fs     //this lets us return better results
-                                    .Query(q2 => q2
-                                        .MultiMatch(m => m
-                                            .Fields(f => f  // search the term on these fields:
-                                                .Field(doc => doc.ReviewTitle, boost: 2.5)      // add boosting to the title
-                                                .Field(doc => doc.ReviewBody)
-                                            )
-                                            .Query(term.ToString())     // the term we're querying
-                                            .Slop(2)                    // character misplacements (typos: avengers=>aevngers)
-                                            .Fuzziness(Fuzziness.Auto)  // token distance (tomato soup=>soup tomatos)
-                                            .PrefixLength(2)            // allows prefixes (
-                                            .MaxExpansions(1)          // limits the amount of docs returned
-                                            .Operator(Operator.Or)
-                                            .Name($"s?={term}")
-                                            .AutoGenerateSynonymsPhraseQuery(false)
-                                            ) && q      // this lets us grab movies based on movieID
-                                            .Term(t => t
-                                                .Field(f => f.MovieID)
-                                                .Value(movie.MovieID)
-                                            )
-                                        )
-                                    .BoostMode(FunctionBoostMode.Multiply)  // boosting settings
-                                    .ScoreMode(FunctionScoreMode.Sum)       // score settings
-                                    .Functions(f => f           // ngl, chatgpt moment
-                                        .Exponential(d => d     // this basically says add a bit of boosting to reviews with higher relevance scores
-                                            .Field(f => f.UsefulnessVote)
-                                            .Decay(0.33)
-                                            .Origin(5000)
-                                            .Scale(5)
-                                            .Offset(1)
-                                            .Weight(0.1)
-                                            )
-                                        )
-                                    )
-                                )
-*/
-
-/*
-
-                            .Query(q =>
-                                WeightedReviewQuery(term, movie)
-                                )
-
-
-*/
