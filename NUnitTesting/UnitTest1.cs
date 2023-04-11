585263a5c40327019481e297a53cc388d1a5a4e0
Michael DAgostino <dagostinomichael00@gmail.com>
2023-04-10 22:03:25 -0400

    Added SearchTest Unit Test. BasicSearchService now returns top generic reviews when there's a match on movie title, but no matches on review contents.

--- NUnitTesting/UnitTest1.cs
+++ NUnitTesting/UnitTest1.cs
@@ -1,8 +1,10 @@
 using API;
 using API.Controllers;
+using API.services;
 using Elasticsearch.Net;
 using Microsoft.AspNetCore.Mvc;
 using Nest;
+using System.Net;
 
 namespace NUnitTesting
 {
@@ -33,5 +35,35 @@ namespace NUnitTesting
             var movies = ((OkObjectResult)response.Result).Value as List<Movie>;
             Assert.That(movies.Count, Is.EqualTo(10));
         }
+
+        [Test]
+        [TestCase("")]
+        [TestCase("avengers")]
+        [TestCase("aven")]
+        [TestCase("good movie")]
+        [TestCase("asovuboaiwba09")]
+        [TestCase("{}!@sdoih';';......-=-=:::")]
+        public async Task SearchTest(string? term)
+        {
+            var response = await _movieController.GetMovieReviewFromTerm(term);
+
+            //Assert
+            // Assert we have a response.
+            Assert.NotNull(response.Result);
+
+            // Assert Status Code 200 is returned.
+            Assert.IsInstanceOf<OkObjectResult>(response.Result);
+
+            // Assert that we have MovieDocuments always being returned;
+            var movies = ((OkObjectResult)response.Result).Value as MovieReview;
+            Assert.That(movies.MovieDocuments.Count(), Is.GreaterThan(0));
+
+            // If we matched on a movie title, assert that we are getting reviews back.
+            if(movies.MovieDocuments.Count() < 10)
+            {
+                Assert.That(movies.MovieDocuments.Count(), Is.EqualTo(movies.ReviewDocuments.Count()));
+            }
+        }
+
     }
 }
