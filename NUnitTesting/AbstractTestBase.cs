86702924f949cd80c8affcd82ce95d66793366be
Michael DAgostino <dagostinomichael00@gmail.com>
2023-04-11 14:05:29 -0400

    Moved Get10MoviesTest to its own class.

--- NUnitTesting/AbstractTestBase.cs
+++ NUnitTesting/AbstractTestBase.cs
@@ -9,7 +9,7 @@ using System.Net;
 namespace NUnitTesting
 {
     [TestFixture]
-    public class AbstractTestBase
+    public abstract class AbstractTestBase
     {
         private IElasticClient _elasticClient;
         protected MoviesController _movieController;
@@ -21,21 +21,5 @@ namespace NUnitTesting
             _elasticClient = new ElasticClient();
             _movieController = new MoviesController(_elasticClient);
         }
-        
-        /*
-        [Test]
-        public async Task Get10MoviesTest()
-        {
-            // Act
-            var response = await _movieController.GetMovieList();
-
-            // Assert
-            Assert.NotNull(response.Result);
-            Assert.IsInstanceOf<OkObjectResult>(response.Result);
-
-            var movies = ((OkObjectResult)response.Result).Value as List<Movie>;
-            Assert.That(movies.Count, Is.EqualTo(10));
-        }
-        */
     }
 }
