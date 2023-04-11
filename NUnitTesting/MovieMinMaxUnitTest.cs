e69e86c2b909abc5cce16047e1be2fdcb03a139a
Robert McNiven <robertmcnivenjr@gmail.com>
2023-04-11 15:28:31 -0400

    Made TestMinMaxContents to make sure correct type

--- NUnitTesting/MovieMinMaxUnitTest.cs
+++ NUnitTesting/MovieMinMaxUnitTest.cs
@@ -3,11 +3,8 @@ namespace NUnitTesting
     internal class MovieMinMaxUnitTest : AbstractTestBase
     {
         [Test]
-        [TestCase(null, null, null)]
-        [TestCase('movieIMDbRating', null, null)]
-        [TestCase(null, 1.3, null)]
-        [TestCase(null, null, 8.6)]
-        [TestCase('totalRatingCount', 100000, 2000000)]
+        [TestCase("movieIMDbRating", 1.3, 8.6)]
+        [TestCase("totalRatingCount", 100000, 2000000)]
 
 
         public async Task TestMinMaxResponse(string field, float minNum, float maxNum)
@@ -19,8 +16,21 @@ namespace NUnitTesting
             Assert.NotNull(response.Result);
             Assert.IsInstanceOf<OkObjectResult>(response.Result);
 
-            var movies = ((OkObjectResult)response.Result).Value as List<Movie>;
-            Assert.That(movies.Count, Is.EqualTo(10));
+            //var movies = ((OkObjectResult)response.Result).Value as List<Movie>;
+           // Assert.That(movies.Count, Is.EqualTo(10));
+        }
+
+        [Test]
+        [TestCase("movieIMDbRating", 1.3, 8.6)]
+        [TestCase("totalRatingCount", 100000, 2000000)]
+        public async Task TestMinMaxContetnt(string field, float minNum, float maxNum){
+            // Act
+            var response = await _movieController.GetMovieReviewFromMinMax(field, minNum, maxNum);
+
+            // Assert
+            Assert.NotNull(response.Result);
+
+            Assert.IsInstanceOf<List<Movie>>(((OkObjectResult)response.Result).Value);
         }
     }
 }
